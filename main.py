854db1262efa8c8854f1d1793ce6659e73b9942f
independent mind <46705473+independent-mind@users.noreply.github.com>
2022-11-20 20:48:42 +0000

    More detailed information about BMI.

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-height = input("enter your height in m: ")
-weight = input("enter your weight in kg: ")
+height = float(input("Enter your height in m: "))
+weight = float(input("Enter your weight in kg: "))
 
-BMI = int(weight) / ((float(height) * float(height)))
-result = int(BMI)
-print(result)
+BMI = weight / (height ** 2)
+
+result = int(round(BMI, 0))
+
+if result < 18.5:
+  print(f"Your BMI is {result}, you are underweight.")
+elif result < 25:
+  print(f"Your BMI is {result}, you have a normal weight.")
+elif result < 30:
+  print(f"Your BMI is {result}, you are slightly overweight.")
+elif result < 35:
+  print(f"Your BMI is {result}, you are obese.")
+else:
+  print(f"Your BMI is {result}, you are clinically obese.")
+  
